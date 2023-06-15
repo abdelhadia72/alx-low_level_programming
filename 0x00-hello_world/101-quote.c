e60e5324799c0ce25491f715c415586e0a2a48b4
abdelhadia72 <thisisavnb@gmail.com>
2023-06-15 12:43:09 -0700

    0x00

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-#include <unistd.h>
+dd#include <unistd.h>
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 1 (indicating an error condition)
- */
+* main - Entry
+*
+* Return: 1 
+*/
+
 int main(void)
 {
-	const char message[];
-	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fputs(message, stderr);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
