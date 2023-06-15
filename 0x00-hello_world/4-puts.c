877b1191c75a663df364068bbe0eebc0d3c58d8d
abdelhadia72 <thisisavnb@gmail.com>
2023-06-15 05:13:40 -0700

    0x00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point 
- *
- * Description: This function prins a message to the console.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
