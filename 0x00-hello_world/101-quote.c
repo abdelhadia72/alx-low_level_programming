9aa3b84b4a4431d7bedabfd5080f1aa1a5167c93
abdelhadia72 <thisisavnb@gmail.com>
2023-06-15 06:04:07 -0700

    0x00

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,8 @@ int main(void)
 {
 #include <unistd.h>
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-    write(STDERR_FILENO, message, sizeof(message) - 1);
-    return 1;
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+   	fputs(message, stderr);
+	return 1;
 }
 
