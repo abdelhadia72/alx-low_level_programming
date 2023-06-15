f6dca2a6fe7b081247dd1e5e152c271664ee353a
abdelhadia72 <thisisavnb@gmail.com>
2023-06-15 07:00:24 -0700

    0x00

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - Entry
+ * main - Entry point of the program
  *
- * Return: Always 1 
+ * Return: Always 1 (indicating an error condition)
  */
 int main(void)
 {
 	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	fputs(message, stderr);
-	return 1;
+	return (1);
 }
 
