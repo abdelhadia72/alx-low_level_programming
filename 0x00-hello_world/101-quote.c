caf2626f87d0fd11dca6ccfde1a9b7c6c07ba28c
abdelhadia72 <thisisavnb@gmail.com>
2023-06-15 12:54:36 -0700

    0x00

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
-* main - Entry
-*
-* Return: 1 
-*/
-
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
