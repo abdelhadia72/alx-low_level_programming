28ee04110a9df93f3567fc6b74f9dbd73725b5e8
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-16 19:49:21 +0100

    0x01

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-int i,j,k;
-for (i=0; i<10 ; i++)
+int i, j;
+for (i = 0; i < 10; i++)
 {
-    for (j=0; j<10; j++)
-    {
-
-    putchar('0' + i);
-
-    }
+for (j = 0 ; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar(44);
+putchar(' ');
 }
-
+}
 return (0);
 }
