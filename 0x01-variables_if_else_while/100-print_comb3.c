bac35ac1604b3b080d0cae60476aaced38f65373
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-16 19:39:51 +0100

    0x01

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (No Error)
+ */
+int main(void)
+{
+int i,j,k;
+for (i=0; i<10 ; i++)
+{
+    for (j=0; j<10; j++)
+    {
+
+    putchar('0' + i);
+
+    }
+}
+
+return (0);
+}
