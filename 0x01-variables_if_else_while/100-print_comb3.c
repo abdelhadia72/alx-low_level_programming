88ea22e20d865a54cf60642ffc9a092df7dc46a0
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-16 21:17:56 +0100

    0x01

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,14 @@ int main(void)
 int i, j;
 for (i = 0; i < 9; i++)
 {
-for (j = i+1; j < 10; j++)
+for (j = i + 1; j < 10; j++)
 {
 putchar('0' + i);
 putchar('0' + j);
-if (i != 8 || j != 9 )
+if (i != 8 || j != 9)
 {
 putchar(44);
 putchar(' ');
-
 }
 }
 }
