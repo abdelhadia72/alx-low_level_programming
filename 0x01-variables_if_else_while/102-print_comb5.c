88b0f95de30e09e280e7f919fdec9a1a702784fb
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-17 01:54:55 +0100

    0x01

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: 0 (No Error)
  */
 int main(void)
 {
+	int i, j;
 
-printf("Hi !");
-return 0;
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			if (j != 0)
+			{
+				putchar(' ');
+			}
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+			}
+
+		}
+	}
+
+	return (0);
 }
+
