51012e674349add32dee88105d3a5733f2e7a318
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-20 19:09:00 +0100

    0x02

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Funtion that return a + b
+ * Description: function that sum two
+ * numbers
+ * Return: a + b sum them
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int add(int, int);
 void times_table(void);
 void jack_bauer(void);
 int print_last_digit(int);
