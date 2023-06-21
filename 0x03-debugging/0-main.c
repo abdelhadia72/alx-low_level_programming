abeccaf3eb0587d59a989d35638a58f108479943
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-21 13:00:32 +0100

    0x03

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,10 @@ include "main.h"
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
