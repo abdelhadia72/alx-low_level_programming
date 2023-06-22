337ee0dc1ec469253c1a993524fe7047a7298276
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-22 21:29:29 +0100

    0x04

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entery Point
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int x = 612852475143;
+	long int py;
+
+	for (py = 2; py < x; py++)
+	{
+		if (x % py == 0)
+		{
+			x = x / py;
+		}
+	}
+	printf("%ld\n", py);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+/**
+ * print_number - print number
+ * @n: the arrg that gived by
+ * the user to print it .
+ * Return: void
+ */
+
+void print_number(int n)
+{
+
+	unsigned int n1 = 0;
+
+	if  (n < 0)
+	{
+		n1 = -n;
+		putchar('-');
+	}
+
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	putchar((n1 % 10) + '0');
+}
