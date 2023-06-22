2b671d17c3757f5cc6dfbd9902d1dab9a0cb54b3
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-22 10:52:21 +0100

    0x04

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
+char _putchar(char c);
+#endif
