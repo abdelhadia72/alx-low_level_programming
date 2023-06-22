bacd0c7949a321d4c1e98affa036bca101855d1b
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-22 11:12:11 +0100

    0x04

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - function print diagonal
+ * Description: This function prints
+ * diagnaol
+ * @n: the argument that holde the tall of
+ * the diagnaol
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+	{
+		_putchar (32);
+	}
+	_putchar (92);
+	_putchar ('\n');
+	}
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+}
