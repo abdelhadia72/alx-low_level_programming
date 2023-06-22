ecdd1205da9130283d78bc06bacad3c6581e21e2
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-22 11:04:50 +0100

    0x04

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - function print numbers
+ * print_numbers - function print numbers
  * Description: This function prints
  * all numbers exept 2 and 4
  * Return: void
