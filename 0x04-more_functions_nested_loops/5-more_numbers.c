db1d0568f57f784e7cf7eefcaaf1bf164b9b46ab
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-22 10:59:17 +0100

    0x04

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+v#include "main.h"
+
+/**
+ * more_numbers - function print numbers
+ * Description: This function print from 0
+ * to 14 using only _putchar
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+				_putchar((j % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
