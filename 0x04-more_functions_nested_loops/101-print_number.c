eebedff06003a482baf1aacd37b608b73d59f554
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-22 21:34:57 +0100

    0x04

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,7 +20,7 @@ void print_number(int n)
 	if  (n < 0)
 	{
 		n1 = -n;
-		putchar('-');
+		_putchar('-');
 	}
 
 	else
@@ -31,5 +33,5 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
