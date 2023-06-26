55c7a0e33b7d2e97fd262e6883190c46cb5c6806
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-26 22:54:29 +0100

    0x05

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_array - funtion prints array
+ * Description: This funtion
+ * prints all element that do you have in
+ * your array
+ * @a: the array giving by the user
+ * @n: the size of the array
+ * Return: void
+ *
+ */
+
+
 void print_array(int *a, int n)
 {
 	int i;
