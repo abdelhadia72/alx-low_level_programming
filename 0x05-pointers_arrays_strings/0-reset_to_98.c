26c849f9e1b7b8cc47298a0c791d743fb6a1a5d7
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-26 06:03:59 +0100

    0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - funtion reset
+ * Description: This funtion reset
+ * any number that giving by @n to  98
+ * @n: the arrg that giving
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
