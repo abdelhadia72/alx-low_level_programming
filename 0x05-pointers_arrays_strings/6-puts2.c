95590ddb3de0e8ec0a15b0dbd5575910914fbf90
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-26 16:24:00 +0100

    0x05

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,17 +12,15 @@
 
 void puts2(char *str)
 {
-
 	int i;
 	i = 0;
-	while (*str != '\0')
+	while (str[i] != '\0')
 	{
-		if (i % 2 == 44)
+		if (i % 2 == 0)
 		{
-			putchar(*str);
+			putchar(str[i]);
 		}
-
-		str++;
+	i++;
 	}
 	putchar('\n');
 }
