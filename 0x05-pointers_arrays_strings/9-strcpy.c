f54f0cf4e82529b6ecba3968509caf1c0d7088b0
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-27 00:10:02 +0100

    0x05

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strcpy(char *dest,char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = *(src + i);
+
+	return dest;
+}
