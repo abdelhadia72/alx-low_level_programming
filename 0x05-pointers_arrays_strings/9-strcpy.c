df055a0eecc3dacc817c6ba894ea8a670cf9dd78
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-27 00:18:20 +0100

    0x05

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strcpy - funtion copy string
+ * Description: This funtion
+ * it's copy a string from the @src
+ * @src: the string giving by the user
+ * @dest: the arrg that we copy to .
+ * Return: dest
+ *
+ */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
