03cbd92636259906cd93884770b6bfd4615ba30e
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-26 07:00:52 +0100

    0x05

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _puts - funtion prints string
+ * Description: This funtion that
+ * print a string for you
+ * @str: the arrg that's giving by
+ * the user
+ * Return: void
+ *
+ */
+
 void _puts(char *str)
 {
 	while (*str != '\0')
