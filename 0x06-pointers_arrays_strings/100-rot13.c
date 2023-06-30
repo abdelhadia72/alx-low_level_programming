afeb0cb4ae57e30580df35d6b265c98bf9d30504
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-30 01:48:36 +0100

    0x06 + 🐏

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* rot13 - rot13 encryption algorithm
+* Description:  function that encodes a string using rot13.
+* @str: string to be encrypted
+* Return: rot13 encoded 
+*/
+
+char *rot13(char *str)
+{
+	int i, j;
+	char firstRot13[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char secondRot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (str[i] != '\0')
+	{
+	{
+		for (j = 0; firstRot13[j] != '\0'; j++)
+		{
+			if (str[i] == firstRot13[j])
+			{
+				str[i] = secondRot13[j];
+				break;
+			}
+		}
+	}
+	i++;
+	}
+	return (str);
+}
