bd6878ce1c46c9639c97bc583edf3480ba2d955c
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-30 01:57:26 +0100

    0x06 + 🐏

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_number - prints numbers
+  * @n: Number to prints
+  * Return: void
+  */
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	m = n;
+
+	if (m / 10)
+		print_number(m / 10);
+
+	_putchar(m % 10 + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
