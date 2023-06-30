3c9bfaa4a468deb25393434e3998ffb7c245e4d0
AbdelahdiA72 <thisisavnb@gmail.com>
2023-06-30 01:54:30 +0100

    0x06 + 🐏

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,19 +7,18 @@
 */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int m;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
+		n *= -1;
 	}
 
-	if (n1 / 10)
-		print_number(n1 / 10);
+	m = n;
 
-	_putchar((n1 % 10) + '0');
+	if (m / 10)
+		print_number(m / 10);
+
+	_putchar(m % 10 + '0');
 }
