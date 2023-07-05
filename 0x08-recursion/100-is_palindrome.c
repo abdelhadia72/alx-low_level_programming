a395e933583863d3eece748a3eaa8053ff77a0d5
AbdelahdiA72 <thisisavnb@gmail.com>
2023-07-05 08:34:15 +0100

    ⛽ 0x08

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,51 @@
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The input string.
+ *
+ * Return: 1 if @s is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	int size;
+
+	size = get_size(s);
+	return (check_palindrome(s, s + size - 1));
+}
+
+/**
+ * get_size - Calculates the size of a string.
+ * @s: The input string.
+ *
+ * Return: The size of the string.
+ */
+int get_size(char *s)
+{
+	int size;
+
+	size = 0;
+
+	if (*s != 0)
+		return (size += get_size(s + 1) + 1);
+	return (0);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @start: Pointer to the start of the string.
+ * @end: Pointer to the end of the string.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int check_palindrome(char *start, char *end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	else if (*start != *end)
+	{
+		return (0);
+	}
+	return (check_palindrome(start + 1, end - 1));
+}
+
