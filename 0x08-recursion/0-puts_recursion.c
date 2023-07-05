0f205574df5bd0297d06292cea9cff921bd8f6e9
AbdelahdiA72 <thisisavnb@gmail.com>
2023-07-05 07:39:23 +0100

    ⛽ 0x08

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,12 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		putchar(*s);
+		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
 	else
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 }
-
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
