458a48423150a68bc7224e5ac859f39a42c1c92a
AbdelahdiA72 <thisisavnb@gmail.com>
2023-07-07 14:59:01 +0100

    🧭 0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n",argc-1);
+	return 0;
+}
