e6522a11db94ff0f0d47d88bb9fbbf4474d30568
AbdelahdiA72 <thisisavnb@gmail.com>
2023-07-07 18:07:57 +0100

    🧭 0x0A

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: 0 if these no error
+ * and 1 if these one
+ */
+
+int main(int argc, char **argv)
+{
+
+	int i, j;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+	} else
+	{
+		printf("0\n");
+	}
+
+	for (j = 1; j < argc; j++)
+	{
+		printf("1");
+	}
+	printf("\n");
+	return (0);
+}
