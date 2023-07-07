bec7f701dc86316c575737f907df9f6624ae06cb
AbdelahdiA72 <thisisavnb@gmail.com>
2023-07-07 14:51:50 +0100

    🧭 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", *argv);
+	return (0);
+}
