e7d0dff4f14a1deb655cecb27a62b987b64296b5
abdelhadia72 <thisisavnb@gmail.com>
2023-07-13 16:35:32 +0100

    🌅 0X0C

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,25 +3,25 @@
 #include <string.h>
 #include "main.h"
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *tmp;
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
+  void *tmp;
 
-	if (new_size == 0) {
-		free(ptr);
-		return NULL;
+  if (new_size == old_size) {
+	return ptr;
+  } else if (ptr == NULL) {
+	tmp = malloc(new_size);
+  } else if (new_size == 0) {
+	free(ptr);
+	return NULL;
+  }
+  else {
+	tmp = malloc(new_size);
+	if (tmp == NULL) {
+	  return NULL;
 	}
+	memcpy(tmp, ptr, old_size);
+	free(ptr);
+  }
 
-	if (ptr == NULL) {
-		ptr = malloc(new_size);
-	} else {
-		tmp = malloc(new_size);
-		if (tmp == NULL) {
-			return NULL;
-		}
-		memcpy(tmp, ptr, old_size);
-		free(ptr);
-	}
-
-	return tmp;
+  return tmp;
 }
