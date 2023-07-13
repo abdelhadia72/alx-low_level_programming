54b94e50464683ff4aeedde373196adbfba8337e
abdelhadia72 <thisisavnb@gmail.com>
2023-07-13 12:42:12 +0100

    🌅 0X0C

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,6 +9,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if(old_size >= new_size)
 		return malloc(new_size);
 
+	if(new_size == old_size)
+		return ptr;
+
 	if(new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
