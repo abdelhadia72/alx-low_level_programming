f2aa0e72afe76f6768a79634d981819bb1e15e86
abdelhadia72 <thisisavnb@gmail.com>
2023-07-17 17:28:33 +0100

    🔭 0X0E

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+
+	if (d)
+	{
+		(d->name) ? printf("%s\n", d->name) : printf("nil\n");
+		printf("%f\n", d->age);
+		(d->owner) ? printf("%s\n", d->owner) : printf("nil\n");
+	}
+
+}
