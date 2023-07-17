a59ed99c9e7576170796b28a5d3c1d8b9c7816f5
abdelhadia72 <thisisavnb@gmail.com>
2023-07-17 17:00:40 +0100

    🔗 0X0D

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog info
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
