0cd05678cc493283042f1a38238018bc254ba716
abdelhadia72 <thisisavnb@gmail.com>
2023-07-17 18:02:15 +0100

    🔭 0X0E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @age: age of the dog
  */
 
-struct dog
+typedef struct
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
