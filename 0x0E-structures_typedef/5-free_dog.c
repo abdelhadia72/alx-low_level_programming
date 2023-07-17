c3458cda2ffc876f0711848baffbb62d78bfd2cc
abdelhadia72 <thisisavnb@gmail.com>
2023-07-17 18:42:42 +0100

    🔗 0X0D

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees memory allocated for a dog structure
+ * @d: Pointer to the dog structure
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		else if (d->owner)
+			free(d->owner);
+
+		free(d);
+	}
+}
