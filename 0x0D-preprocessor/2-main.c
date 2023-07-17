e81757b2eab3ac600553452ca759c6753fcc543a
abdelhadia72 <thisisavnb@gmail.com>
2023-07-17 16:38:09 +0100

    🔗 0X0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ * Description: funtion that
+ * print the name of the file
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
