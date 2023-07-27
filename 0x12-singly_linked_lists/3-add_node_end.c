44c9c3fb3f71e7d92339b0f95f080fe1e8e91099
abdelhadia72 <thisisavnb@gmail.com>
2023-07-27 22:55:25 +0100

    function adding new node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_node_end - Adds a new node at the end of a linked list.
+ * @head: Pointer to the head of the list.
+ * @str: String to be stored in the new node.
+ * Return: Address of the new node, or NULL if failed.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp;
+	list_t *newNode = (list_t *)malloc(sizeof(list_t));
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	if (!newNode->str)
+		return (NULL);
+
+	newNode->len = strlen(str);
+	newNode->next = NULL;
+
+	if (!*head)
+	{
+		*head = newNode;
+	} else
+	{
+		tmp = *head;
+		while (tmp->next)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = newNode;
+	}
+
+	return (newNode);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,6 +13,7 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
