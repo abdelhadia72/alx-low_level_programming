e042933b5c1ab130b9b2984bd6ec77243ba53272
abdelhadia72 <thisisavnb@gmail.com>
2023-07-27 17:17:11 +0100

    add betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_list - Prints the elements of a linked list.
+ * @h: Pointer to the head node of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
