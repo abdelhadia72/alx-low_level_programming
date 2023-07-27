421f302b29a6dffa501253dd98a942b2f9ced09a
abdelhadia72 <thisisavnb@gmail.com>
2023-07-27 23:35:33 +0100

    fixing header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,6 +13,7 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+void free_list(list_t *head);
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
