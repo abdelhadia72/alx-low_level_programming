034d7a612d8fed4538e254a30ff67e723e4912f1
abdelhadia72 <thisisavnb@gmail.com>
2023-07-28 00:49:47 +0100

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,9 @@
-section .data
-	hello_msg db 'Hello, Holberton', 0
-	format db '%s', 0
-	newline db 10, 0
-
-section .text
-	global main
-
-extern printf
-
+		  global    main
+		  extern    printf
 main:
-	push rbp
-	mov rdi, format
-	mov rsi, hello_msg
-	call printf
-	mov rdi, format
-	mov rsi, newline
-	call printf
-	pop rbp
-	ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
