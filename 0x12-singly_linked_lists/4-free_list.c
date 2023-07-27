9b5fc8fa9d9c7a33bd5b6bc142767a89953e23a8
abdelhadia72 <thisisavnb@gmail.com>
2023-07-27 23:39:56 +0100

    fixing free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,8 +1,14 @@
 #include "lists.h"
 
+/**
+ * free_list - Frees a linked list including its strings.
+ * @head: Pointer to the head of the list.
+ */
+
 void free_list(list_t *head)
 {
 	list_t *holder;
+
 	while (head)
 	{
 		holder = head;
