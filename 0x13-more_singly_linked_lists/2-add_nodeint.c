fc54d711e8dbf304142d47ba32a3c93b3a29d32a
abdelhadia72 <thisisavnb@gmail.com>
2023-07-31 19:43:09 +0100

    fix task 1,2,3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,15 +1,24 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @n: The data value to be stored in the new node.
+ *
+ * Return: Address of the new node, or NULL if allocation fails.
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	if(!new_node)
+
+	if (!new_node)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = (*head);
 	(*head) = new_node;
-	
+
 	return (new_node);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,20 +11,20 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	
+
 	listint_t *current;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
 	if (!new_node)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = NULL;
-	
-	if(!(*head))
+
+	if (!(*head))
 	{
 		*head = new_node;
-	} 
+	}
 	else
 	{
 		current = *head;
@@ -34,5 +34,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		current->next = new_node;
 	}
+
 	return (new_node);
 }
