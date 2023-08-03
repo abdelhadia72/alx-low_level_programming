23ee152fa160e72822ae6ab4e24c45fdad9040ed
abdelhadia72 <thisisavnb@gmail.com>
2023-08-03 20:19:15 +0100

    fix 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,11 +15,9 @@ unsigned int binary_to_uint(const char *b)
 	while (b[i])
 	{
 		if (b[i] != '0' && b[i] != '1')
-		{
-			value = 0;
-			break;
-		}
-		value = (value << 1) + (b[i] - 48);
+			return = 0;
+			
+		value = (value << 1) + (b[i] - '0');
 		i++;
 	}
 	return (value);
