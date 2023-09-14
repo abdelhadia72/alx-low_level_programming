eba322ad1b34e962bd7400d3c856bafa165d2701
abdelhadia72 <thisisavnb@gmail.com>
2023-09-15 00:26:27 +0100

    fix 'insert dnodeint at index'

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,45 +1,55 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new
- * node at a given index of a doubly linked list
+ * insert_dnodeint_at_index - Inserts a new node at a given index of a doubly linked list
  * @h: Pointer to a pointer to the head of the list
  * @idx: Index at which the new node should be inserted
  * @n: Integer value to be stored in the new node
  *
  * Return: Address of the new node or NULL if it fails
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
-	dlistint_t *current = *h;
+    unsigned int i = 0;
+    dlistint_t *newNode;
+    dlistint_t *current = *h;
 
-	if (!h)
-		return (NULL);
+    if (!h)
+        return (NULL);
+
+    newNode = malloc(sizeof(dlistint_t));
+    if (!newNode)
+        return (NULL);
+
+    newNode->n = n;
+
+    if (idx == 0)
+    {
+        newNode->prev = NULL;
+        newNode->next = *h;
+        if (*h)
+            (*h)->prev = newNode;
+        *h = newNode;
+        return (newNode);
+    }
 
 
-	if (!newNode)
-		return (NULL);
+    while (current)
+    {
+        if (i == idx - 1)
+        {
+            newNode->prev = current;
+            newNode->next = current->next;
+            if (current->next)
+                current->next->prev = newNode;
+            current->next = newNode;
+            return (newNode);
+        }
+        current = current->next;
+        i++;
+    }
 
-	newNode->n = n;
-	newNode->prev = NULL;
-	newNode->next = *h;
-
-
-	while (current)
-	{
-		if (i == idx - 1)
-		{
-			newNode->prev = current;
-			newNode->next = current->next;
-			current->next->prev = newNode;
-			current->next = newNode;
-		}
-		current = current->next;
-		i++;
-	}
-
-	return (newNode);
+    free(newNode);
+    return (NULL);
 }
+
