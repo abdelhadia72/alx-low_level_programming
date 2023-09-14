a827dce6ea6b265be71df8ebdaf58346d8953a2c
abdelhadia72 <thisisavnb@gmail.com>
2023-09-14 20:04:26 +0100

    sum all dlinked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Calculates the sum of all integer
+ * values in a doubly linked list
+ * @head: Pointer to the head of the list
+ *
+ * Return: Sum of all integer values in the list,
+ * or 0 if the list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+
+	int sum = 0;
+
+	if (!head)
+		return (0);
+
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
