09085b4ad7fcdaffbdee063617c4cb11b0ececc9
abdelhadia72 <thisisavnb@gmail.com>
2023-09-14 19:04:50 +0100

    fix style 'add dnodeint at the end'

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,29 +1,37 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint_end - Adds a new node at the end of a doubly linked list
+ * @head: Pointer to a pointer to the head of the list
+ * @n: Integer value to be stored in the new node
+ *
+ * Return: Address of the new node or NULL if it fails
+ */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *newNode = malloc(sizeof(dlistint_t));
-    dlistint_t *current;
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+	dlistint_t *current;
 
-    if (!newNode)
-        return (NULL);
+	if (!newNode)
+		return (NULL);
 
-    newNode->n = n;
-    newNode->prev = NULL;
-    newNode->next = NULL;
+	newNode->n = n;
+	newNode->prev = NULL;
+	newNode->next = NULL;
 
-    if (!*head)
-    {
-        *head = newNode;
-        return (newNode);
-    }
+	if (!*head)
+	{
+		*head = newNode;
+		return (newNode);
+	}
 
-    current = *head;
-    while (current->next)
-        current = current->next;
+	current = *head;
+	while (current->next)
+		current = current->next;
 
-    current->next = newNode;
-    newNode->prev = current;
+	current->next = newNode;
+	newNode->prev = current;
 
-    return (newNode);
+	return (newNode);
 }
