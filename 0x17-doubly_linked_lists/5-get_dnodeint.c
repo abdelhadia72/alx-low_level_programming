422d5be7af57e45aeec4f356d35ba77e48661547
abdelhadia72 <thisisavnb@gmail.com>
2023-09-14 19:26:09 +0100

    fixing get nth dnodeint

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *current = head;
 	unsigned int i = 0;
 
-	if (!index)
+	if (!index || !head)
 		return (NULL);
 
 	while (i < index)
