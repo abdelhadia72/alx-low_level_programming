5e633522a705303a4178ab34650cfee624775061
abdelhadia72 <thisisavnb@gmail.com>
2023-09-14 20:31:17 +0100

    fix get nth node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,12 +11,15 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (head)
 	{
 		if (i == index)
 			return (head);
+		head = head->next;
+		i++;
 	}
 
 	return (NULL);
+}
