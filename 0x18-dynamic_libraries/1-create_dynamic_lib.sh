de5f5cddcfffa170dd8f22268b879cec262babf5
abdelhadia72 <thisisavnb@gmail.com>
2023-09-18 16:08:51 +0100

    add ops.so

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 gcc -Wall -Werror -Wextra -pedantic *.c -c -fpic
 gcc -Wall -Werror -Wextra -pedantic *.o -shared -o liball.so
 rm *.o
---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,38 @@
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int mul(int a, int b)
+{
+    return a * b;
+}
+
+int div(int a, int b)
+{
+    if (b != 0)
+    {
+        return a / b;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int mod(int a, int b)
+{
+    if (b != 0)
+    {
+        return a % b;
+    }
+    else
+    {
+        return 0;
+    }
+}
