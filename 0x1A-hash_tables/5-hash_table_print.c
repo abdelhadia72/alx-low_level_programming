556a9b9829b35a31d2656429e03a8922742fb43a
abdelhadia72 <thisisavnb@gmail.com>
2023-10-06 04:04:11 +0100

    print all hashtable

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints the key-value pairs of a hash table.
+ * @ht: A pointer to the hash table.
+ *
+ * Description: This function prints the key-value pairs of a hash table
+ * in the format: '{'key': 'value', 'key': 'value', ...'}'.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	int ones = 0;
+	hash_node_t *node;
+
+	if (!ht)
+		exit(EXIT_FAILURE);
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			if (ones)
+				printf(", ");
+			node = ht->array[i];
+			printf("'%s': '%s'", node->key, node->value);
+			ones = 1;
+		}
+	}
+
+	printf("}\n");
+}
