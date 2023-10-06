6690c963596a415b017081eb31c8576f08db4120
abdelhadia72 <thisisavnb@gmail.com>
2023-10-06 12:41:27 +0100

    FIXING task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,33 +9,33 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL;
+	hash_table_t *ht = NULL;
 	unsigned long int i = 0;
 
 	if (!size)
 		return (NULL);
 
 	/* create a hash table */
-	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
 		return (NULL);
 
 	/* create the array now */
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (new_table->array == NULL)
+	if (ht->array == NULL)
 	{
-		free(new_table);
+		free(ht);
 		return (NULL);
 	}
 
 	/* fill all nodes with null to know the end*/
 	for (i = 0; i < size; i++)
 	{
-		new_table->array[i] = NULL;
+		ht->array[i] = NULL;
 	}
 
 	/* return the new table */
-	return (new_table);
+	return (ht);
 }
