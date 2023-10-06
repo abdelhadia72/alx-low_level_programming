3babc40b71d7632ba4050465f889e22b85068e8f
abdelhadia72 <thisisavnb@gmail.com>
2023-10-06 03:19:45 +0100

    fixing task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,7 @@ hash_node_t *create_new_node(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int current_idx;
-	hash_node_t *current_node, *new_node;
+	hash_node_t *new_node;
 
 	if (!ht || !key || !value)
 		return (0);
